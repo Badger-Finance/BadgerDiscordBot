eaf598e7721c07c14a9c65345546fe07b109b8a3
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-04-29 01:44:50 -0400

    remove unused code

--- scripts/run_honey_badger.py
+++ scripts/run_honey_badger.py
@@ -12,35 +12,6 @@ from honey_badger import BadgerBot
 load_dotenv()
 
 
-def setup_sqs():
-    with mock_sqs():
-        sqs_client = boto3.client("sqs")
-        sqs_client.create_queue(QueueName="sourcecred-registration-requests")
-        yield sqs_client
-
-
-def setup_dynamodb():
-    with mock_dynamodb2():
-        dynamodb_client = boto3.client("dynamodb")
-        dynamodb_client.create_table(
-            TableName="sourcecred-registration",
-            AttributeDefinitions=[
-                {"AttributeName": "discord_id", "AttributeType": "S"},
-            ],
-            KeySchema=[{"AttributeName": "discord_id", "KeyType": "HASH"}],
-        )
-
-        dynamodb_resource = boto3.resource("dynamodb")
-
-        yield dynamodb_client, dynamodb_resource
-
-
-@setup_sqs
-@setup_dynamodb
-def main():
-    pass
-
-
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
